d3365cda1a53d9327a71765338d3849c550f62f4
Kyuwoong Lee <kwooler@gmail.com>
2022-10-12 00:11:13 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,11 @@
-
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
 
 int main()
 {
-	int N=3;
-	int M=3;
-	
-	for(int i=0; i<N; i++)
-	{
-		for (int j=0; j<M; j++)
-		{
-			
-		}
-	}
-
 	
 	// Complete this code.
 	
