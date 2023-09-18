7e0cd8a4f1612df852ed4d1fbd8cccf2b3f15199
Kyu Lee <kwooler@gmail.com>
2023-09-17 18:01:40 -0700

    CS120A57

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-
 using namespace std;
 
 int main()
 {
-	
-	// Complete this code.
-	
+	int n1, n2;
+	int N;
+
+	cin >> n1 >> n2;
+	cin >> N;
+
+	/***************************************************
+	 * Code your program here
+	 **************************************************	*/
 }
