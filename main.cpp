05c1ac36ab8b563bd5fcc0dd78142457aee23cac
karlab21 <karlabarragan03@icloud.com>
2024-09-03 12:12:19 -0700

    main

--- main.cpp
+++ main.cpp
@@ -4,12 +4,19 @@ using namespace std;
 int main()
 {
 	int n1, n2;
-	int N;
+	int N, n3;
+
 
 	cin >> n1 >> n2;
 	cin >> N;
 
-	/***************************************************
-	 * Code your program here
-	 **************************************************	*/
+	cout << n1 << "\t" << n2 << "\t";
+	for (int i=0; i<N-2; i++) 
+		n3= n1 + n2;
+		cout << n3 << "\t";
+		n1 = n2; 
+		n2 = n3;
+
+	
+
 }
